111aedc403516381d6ab2c1406b44594528d7c57
Eunit99 <eunithelp@gmail.com>
2026-05-28 02:18:54 +0100

    chore: migrate test environment from jsdom to happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
     globals: true,
     setupFiles: "./lib/test/setup.ts",
     include: ["lib/**/*.test.{ts,tsx}"],
